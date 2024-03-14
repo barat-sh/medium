c0434956562e881e20ca9db264b80601a333faef
barat-sh <barath.elangovan@icloud.com>
2024-03-14 21:18:54 +0530

    tested empty project

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,4 +6,19 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+
+app.post('/api/login', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.get('/api/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+
+app.get('/api/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
 export default app
